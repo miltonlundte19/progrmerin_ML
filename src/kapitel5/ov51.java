e35470b8f58c853666625e0759e6adc1c059eb92
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2020-10-16 11:27:39 +0200

    10-16

--- src/kapitel5/ov51.java
+++ src/kapitel5/ov51.java
@@ -1,4 +1,17 @@
 package kapitel5;
 
+import javax.swing.*;
+
 public class ov51 {
+    public static void main (String[] arg) {
+        String s = JOptionPane.showInputDialog("n?");
+        int n = Integer.parseInt(s);
+        int summa=0;
+        int k=1;
+        while (k <= n) {
+            summa = summa + k*k;
+            k = k + 1;
+        }
+        JOptionPane.showMessageDialog(null, "summan blir " + summa);
+    }
 }
---
+++ src/kapitel5/ov52.java
@@ -0,0 +1,16 @@
+package kapitel5;
+
+import javax.swing.*;
+
+public class ov52 {
+    public static void main (String[] arg) {
+        String s =JOptionPane.showInputDialog("n?");
+        double n = Double.parseDouble(s);
+        double summa=0;
+        double k=1;
+        while (k <= n) {
+
+        }
+
+    }
+}
