514e54cc05389907bb88eb42ef7aeec331c78856
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2020-10-20 11:09:19 +0200

    10-20

---
+++ src/kapitel5/ov53.java
@@ -0,0 +1,15 @@
+package kapitel5;
+
+import javax.swing.*;
+
+public class ov53 {
+    public static void main(String[] args) {
+        String hh = JOptionPane.showInputDialog("vilken höjd slepte du billen (i meter)");
+        double h = Double.parseDouble(hh);
+        h = h*100;
+        int stuts = 0
+        while (h >= 1) {
+
+        }
+    }
+}
